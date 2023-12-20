1794e6fbc4f9138ff7f1e1462df0de38bcd1f21b
Jean Trujillo <91339890+jeantpdev@users.noreply.github.com>
2023-12-19 23:33:43 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const ctx = document.getElementById('myChart');
+const dona = document.getElementById('myDona');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(dona, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'Red',
+            'Blue'
+          ],
+          datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50],
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+            ],
+            hoverOffset: 4
+          }]
+    }
+  });
+
